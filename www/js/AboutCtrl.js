338222b2e43f958e2e689b773c4655907cb73ec3
Burhan <bern.3759@yahoo.com>
2016-01-24 16:03:55 +0800

    Hide Google Maps - not been used for a moment

--- www/js/AboutCtrl.js
+++ www/js/AboutCtrl.js
@@ -4,31 +4,31 @@ angular.module('cdr.AboutCtrl', [])
     
 //       var options = {timeout: 10000, enableHighAccuracy: true};
 
-      var latLng = new google.maps.LatLng(3.240999, 101.704995);
+//       var latLng = new google.maps.LatLng(3.240999, 101.704995);
 
-      var mapOptions = {
-         center: latLng,
-         zoom: 17,
-         mapTypeId: google.maps.MapTypeId.ROADMAP
-      };
+//       var mapOptions = {
+//          center: latLng,
+//          zoom: 17,
+//          mapTypeId: google.maps.MapTypeId.ROADMAP
+//       };
 
-      $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+//       $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-      google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+//       google.maps.event.addListenerOnce($scope.map, 'idle', function(){
 
-          var marker = new google.maps.Marker({
-              map: $scope.map,
-              animation: google.maps.Animation.DROP,
-              position: latLng
-          });      
+//           var marker = new google.maps.Marker({
+//               map: $scope.map,
+//               animation: google.maps.Animation.DROP,
+//               position: latLng
+//           });      
 
-          var infoWindow = new google.maps.InfoWindow({
-              content: "Here I am!"
-          });
+//           var infoWindow = new google.maps.InfoWindow({
+//               content: "Here I am!"
+//           });
 
-          google.maps.event.addListener(marker, 'click', function () {
-              infoWindow.open($scope.map, marker);
-          });
+//           google.maps.event.addListener(marker, 'click', function () {
+//               infoWindow.open($scope.map, marker);
+//           });
 
-      });
+//       });
 })
