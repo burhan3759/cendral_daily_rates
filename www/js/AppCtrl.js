27e5cf93df6a64cc98eb28271372aa962a42059b
Burhan <bern.3759@yahoo.com>
2016-07-07 04:47:07 +0800

    Added services.js :function to call modal is inside :Now anyone can call the function and use it

--- www/js/AppCtrl.js
+++ www/js/AppCtrl.js
@@ -1,39 +1,39 @@
 angular.module('cdr.AppCtrl', [])
 
-.service('ModalService', function($ionicModal, $rootScope) {
+// .service('ModalService', function($ionicModal, $rootScope) {
   
   
-  var init = function(tpl, $scope) {
+//   var init = function(tpl, $scope) {
 
-    var promise;
-    $scope = $scope || $rootScope.$new();
+//     var promise;
+//     $scope = $scope || $rootScope.$new();
     
-    promise = $ionicModal.fromTemplateUrl(tpl, {
-      scope: $scope,
-      animation: 'slide-in-up'
-    }).then(function(modal) {
-      $scope.modal = modal;
-      return modal;
-    });
+//     promise = $ionicModal.fromTemplateUrl(tpl, {
+//       scope: $scope,
+//       animation: 'slide-in-up'
+//     }).then(function(modal) {
+//       $scope.modal = modal;
+//       return modal;
+//     });
 
-    $scope.openModal = function() {
-       $scope.modal.show();
-     };
-     $scope.closeModal = function() {
-       $scope.modal.hide();
-     };
-     $scope.$on('$destroy', function() {
-       $scope.modal.remove();
-     });
+//     $scope.openModal = function() {
+//        $scope.modal.show();
+//      };
+//      $scope.closeModal = function() {
+//        $scope.modal.hide();
+//      };
+//      $scope.$on('$destroy', function() {
+//        $scope.modal.remove();
+//      });
     
-    return promise;
-  }
+//     return promise;
+//   }
   
-  return {
-    init: init
-  }
+//   return {
+//     init: init
+//   }
   
-})
+// })
 
 .controller('AppCtrl', function($scope, $ionicModal, $state, $ionicPopup, $ionicHistory, $window, $cordovaDialogs,  $ionicPopover, ModalService){
 
--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('cdr', ['ionic', 'cdr.AppCtrl', 'cdr.RatesCtrl', 'cdr.ConversionCtrl', 'cdr.AboutCtrl', 'ngCordova'])
+angular.module('cdr', ['ionic', 'cdr.Services', 'cdr.AppCtrl', 'cdr.RatesCtrl', 'cdr.ConversionCtrl', 'cdr.AboutCtrl', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
