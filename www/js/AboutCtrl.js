8e3eb7d79a7c168e20ea335d0e3f4de1c599b7f2
Burhan <bern.3759@yahoo.com>
2016-07-20 19:42:13 +0800

    Improve about page and home page, remove refresh button

--- www/js/AboutCtrl.js
+++ www/js/AboutCtrl.js
@@ -1,34 +1,11 @@
 angular.module('cdr.AboutCtrl', [])
 
-.controller('AboutCtrl', function($scope, $ionicLoading, $compile, $state, $cordovaGeolocation){
-    
-//       var options = {timeout: 10000, enableHighAccuracy: true};
+.controller('AboutCtrl', function($scope){
+	
+	// Call using button instead of link
+	$scope.call = function(number){
+		console.log(number);
+		window.location.href = 'tel:' + number;
+	};    
 
-//       var latLng = new google.maps.LatLng(3.240999, 101.704995);
-
-//       var mapOptions = {
-//          center: latLng,
-//          zoom: 17,
-//          mapTypeId: google.maps.MapTypeId.ROADMAP
-//       };
-
-//       $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-//       google.maps.event.addListenerOnce($scope.map, 'idle', function(){
-
-//           var marker = new google.maps.Marker({
-//               map: $scope.map,
-//               animation: google.maps.Animation.DROP,
-//               position: latLng
-//           });      
-
-//           var infoWindow = new google.maps.InfoWindow({
-//               content: "Here I am!"
-//           });
-
-//           google.maps.event.addListener(marker, 'click', function () {
-//               infoWindow.open($scope.map, marker);
-//           });
-
-//       });
 })
